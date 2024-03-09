45fcc7699b192e61c74f531bcc6a0c0f2b7fc37b
denoseu <denisefeliciatiowanni@gmail.com>
2024-03-09 15:59:03 +0700

    Update run-bots.sh

--- src/superqueen/run-bots.sh
+++ src/superqueen/run-bots.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 main.py --logic SuperSilverqueen --email=default@email.com --name=super1 --password=defa --team etimo &
-python3 main.py --logic SuperSilverqueen --email=default2@email.com --name=super2 --password=defa22 --team etimo &
-python3 main.py --logic SuperSilverqueen --email=default333@email.com --name=super3 --password=defa2222 --team etimo &
-python3 main.py --logic SuperSilverqueen --email=superqueen@email.com --name=superqueen --password=superqueen --team etimo &
+python3 main.py --logic SuperSilverqueen --email=super1@email.com --name=super1 --password=sia --team etimo &
+python3 main.py --logic SuperSilverqueen --email=super2@email.com --name=super2 --password=aaaa --team etimo &
+python3 main.py --logic SuperSilverqueen --email=super333@email.com --name=super3 --password=tes33 --team etimo &
+python3 main.py --logic SuperSilverqueen --email=superqueen@email.com --name=super4 --password=superqueen --team etimo &
